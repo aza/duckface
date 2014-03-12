0cbbbe15938d7f519c8b2e19fa91bcc66def9f94
Aza <azaaza@gmail.com>
2014-03-12 16:18:15 -0700

    Starting to do face detection

--- tagger/js/tagger.js
+++ tagger/js/tagger.js
@@ -1,14 +1,33 @@
 function Tagger(){
 	var self = this,
-			trainingDataRef = new Firebase('https://duckface.firebaseio.com/training')
+			trainingDataRef = new Firebase('https://duckface.firebaseio.com/training'),
+			faceppKey = '83c6d84242b88722bbd88bb2000af93e',
+			faceppSecret = 'Tuoci57URDmmzD_IArfn3xSVxziVGb9K'
 
+	var faceppUrl = 'http://apius.faceplusplus.com/v2/detection/detect'
+
+
+	function fetchAndSaveFaceppData(id, entry){
+		$.get(faceppUrl, {
+				api_key: faceppKey,
+				api_secret: faceppSecret,
+				url: entry.url
+			}, function(data){
+				var trainedDataRef = trainingDataRef.child(id).child('facepp_detect')
+				trainedDataRef.set( data )
+				console.log( "RETURN DATA", data )
+			})
+	}
 
 	this.populateFaceppJson = function( ){
 		trainingDataRef.on('value', function(snapshot){
 			var entries = snapshot.exportVal()
 
 			_(entries).each(function(value, key){
-				console.log( value, key )
+				if( !value.facepp_detect ){
+					fetchAndSaveFaceppData( key, value )	
+					console.log( value, key )
+				}
 			})
 		})
 	}
@@ -29,14 +48,14 @@ function Tagger(){
 				.attr('src', entry.url)
 				.appendTo('.tagger')
 
-			if( entry.faceppJson ) {
+			if( entry.facepp_detect ) {
 				img.addClass( 'hasFaceppJson' )
 			}
 		})
 	}
 
 	self.getEntries( self._displayEntries )
-	this.getNoDataEntries()
+	self.populateFaceppJson()
 }
 
 
