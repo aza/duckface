2bf58dddbf92e210d2c69085fa0d29466181a011
agong <abe@jawbone.com>
2014-03-12 15:59:04 -0700

    Got push_to_firebase working

---
+++ grab_duckfaces_from_twitter/push_to_firebase.py
@@ -0,0 +1,28 @@
+"""
+http://statigr.am/tag/duckfaces
+//div[@class="image-wrapper"]/a/img/@src
+"""
+
+from firebase import firebase
+fb = firebase.FirebaseApplication('https://duckface.firebaseio.com/')
+
+old = fb.get('/training', None).values()
+old_urls = [row['url'] for row in old if row != None]
+
+new_urls = file('instagram-duckface-8.txt').read().splitlines()
+keep_urls = set(new_urls).difference(set(old_urls))
+
+for i,u in enumerate(keep_urls):
+    print i, u
+    fb.post('/training', {'url': u, 'source': 'instagram#duckface'})
+
+
+old = fb.get('/training', None).values()
+old_urls = [row['url'] for row in old if row != None]
+
+new_urls = file('instagram-selfie-8.txt').read().splitlines()[:20]
+keep_urls = set(new_urls).difference(set(old_urls))
+
+for i,u in enumerate(keep_urls):
+    print i, u
+    fb.post('/training', {'url': u, 'source': 'instagram#selfie'})
